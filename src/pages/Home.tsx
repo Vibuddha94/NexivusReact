14d6e386c6850e2ee5b1e8440dec575f18c2d1d2
Vibuddha94 <bvibuddha@gmail.com>
2024-10-21 18:36:25 +0530

    Create react prroject , Setup rrouting and install Tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+function Home(){
+    return (
+        <div className="bg-violet-400">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home;
